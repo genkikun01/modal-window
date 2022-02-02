71b02a09bad5e5e465f83acd2cac771eedd0d63a
Genki Kun <84325494+genkikun01@users.noreply.github.com>
2022-02-02 22:46:34 +0900

    add new file.

---
+++ index.js
@@ -0,0 +1,24 @@
+// ボタン、モダル、モダルの閉じるボタン、オーバーレイを変数に格納
+const btn = document.querySelector('.btn');
+const modal = document.querySelector('.modal');
+const closeBtn = document.querySelector('.close');
+const overlay = document.querySelector('.overlay');
+
+// ボタンをクリックしたら、モダルとオーバーレイに.activeを付ける
+btn.addEventListener('click', function(){
+  // モーダルとオーバーレイにactiveクラスを付与する
+  modal.classList.add('active');
+  overlay.classList.add('active');
+});
+
+// モダルの閉じるボタンをクリックしたら、モダルとオーバーレイのactiveクラスを外す
+closeBtn.addEventListener('click', function(){
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+});
+
+// オーバーレイをクリックしたら、モダルとオーバーレイのactiveクラスを外す
+overlay.addEventListener('click', function() {
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+});
